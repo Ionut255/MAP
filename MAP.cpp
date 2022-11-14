f054a50883e758fcb72f57ccc190d822f9875146
Ionut Sprincenatu <112524220+Ionut255@users.noreply.github.com>
2022-11-14 22:23:41 +0200

    Nush ce is cu restu de fisiere
    Fisierele o aparut dupa ce am lansat VS - Code din consola

--- MAP.cpp
+++ MAP.cpp
@@ -3,7 +3,28 @@
 
 using namespace std;
 
+int st[100],n,nrsol;
 
+int absolut(int x)
+{
+    if(x<0) 
+    {
+        x=-x;
+        return x;
+    }
+}
+int valid(int k)
+{
+    int i;
+    for ( i = 1; i < k-1; i++)
+    {
+        if ((st[i]==st[k])||(absolut((st[k]-st[i]))==(k-i)))
+        {
+           return 0;
+        }
+    }
+    return 1;
+}
 
 
 int main()
