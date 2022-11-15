76a2382ad2c06d1e57b439ebecc91581320efb6b
Ionut Sprincenatu <112524220+Ionut255@users.noreply.github.com>
2022-11-15 13:07:00 +0200

    L-am facut sa mearga
    Am copiat codu pe care l-am facut pe PC
    si merge, chiar daca era aceelasi cod nush dc nu mergea ;/

--- MAP.cpp
+++ MAP.cpp
@@ -1,78 +1,66 @@
 #include <iostream>
 #include <conio.h>
-
 using namespace std;
-
-int st[100],n,nrsol;
-
+int st[100], n, nrsol;
 int absolut(int x)
 {
-    if(x < 0) 
-        x = -x;
-    return x;
+	if (x < 0) x = -x;
+	return x;
 }
 int valid(int k)
 {
-    int i;
-    for ( i = 1; i < k-1; i++)
-    {
-        if ((st[i]==st[k])||(absolut((st[k] - st[i]))==(k - i)))
-        {
-           return 0;
-        }
-    }
-    return 1;
+	int i;
+	for (i = 1; i <= k - 1; i++)
+		if ((st[i] == st[k]) || (absolut((st[k] - st[i])) == (k - i)))
+			return 0;
+	return 1;
 }
-int sol(int k)
+int solutie(int k)
 {
-    if(k==n)
-        return 1;
-    return 0;
+	if (k == n)
+		return 1;
+	return 0;
 }
+void init(int k) {
+	st[k] = 0;
+}
+void tipar() {
+	int i, j;
+	nrsol++;
+	cout << "\nSolutia "<<nrsol<<" este\n";
 
-void initializare(int k)
-{
-    st[k] = 0;
-}
+		for (i = 1; i <= n; i++)
 
-void tipar()
-{
-    int i,j;
-    nrsol++;
-    cout <<"\n Solutia " << nrsol <<" este: \n";
-    for ( i = 1; i <= n; i++)
-    {
-        cout << endl;
-        for (j = 1; j <=n; j++)
-            if(st[j]==i)
-             cout <<"D";
-            else
-             cout <<"+";
-        
-        
-    }
-    cout <<endl;
+		{
+			cout << endl;
+			for (j = 1; j <= n; j++)
+				if (st[j] == i)
+					cout << " D ";
+				else
+					cout << " + ";
+
+		}
+	cout << endl;
 }
-void back(int k)
-{
-    initializare(k);
-    int i;
-    for(i=1;i<=n;i++)
-    {
-        st[k]=i;
-        if(valid(k)==1)
-        {
-            if(sol(k)==1)
-             tipar();
-            else
-             back(k + 1);
-        }
-    }
+void back(int k) {
+	init(k);
+	int i;
+	for (i = 1; i <= n; i++)
+	{
+		st[k] = i;
+		if (valid(k) == 1)
+		{
+			if (solutie(k) == 1)
+				tipar();
+			else
+				back(k + 1);
+		}
+	}
 }
 int main()
 {
-    n=8;
-    back(1);
-    cout <<"\n Sunt"<<nrsol<<"solutii\n";
- return 0;
+	n=8;
+	back(1);
+	cout << "\nSunt " << nrsol << " solutii";
+	return 0;
 }
